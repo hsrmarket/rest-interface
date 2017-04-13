e03af0fdb2a3dc5732c89e12236916e44aa5c827
Elias Brunner <elias_brunner@gmx.ch>
2017-04-13 10:52:59 +0200

    Electronic class

---
+++ app/models/Electronic.java
@@ -0,0 +1,25 @@
+package models;
+
+
+public class Electronic extends Article {
+
+    private String producer;
+    private String model;
+
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
