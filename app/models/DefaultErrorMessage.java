bba521045522c6a46edbdbe5fbac84833f050ff3
Elias Brunner <elias_brunner@gmx.ch>
2017-04-15 12:11:01 +0200

    Added Constructor

--- app/models/DefaultErrorMessage.java
+++ app/models/DefaultErrorMessage.java
@@ -5,6 +5,10 @@ public class DefaultErrorMessage {
     private Integer id;
     private String message;
 
+    public DefaultErrorMessage(Integer id, String message) {
+        this.id = id;
+        this.message = message;
+    }
 
     public int getId() {
         return id;
--- app/models/DefaultSuccessMessage.java
+++ app/models/DefaultSuccessMessage.java
@@ -5,6 +5,10 @@ public class DefaultSuccessMessage {
     private Integer id;
     private String message;
 
+    public DefaultSuccessMessage(Integer id, String message) {
+        this.id = id;
+        this.message = message;
+    }
 
     public int getId(){
         return this.id;
