cc4767a597425c79caab2ac2d394a7cb6ec52bab
Elias Brunner <elias_brunner@gmx.ch>
2017-04-20 18:46:41 +0200

    updateOneOfficeSupply(id) works

--- app/controllers/OfficeSupplyController.java
+++ app/controllers/OfficeSupplyController.java
@@ -76,6 +76,53 @@ public class OfficeSupplyController extends Controller {
     }
 
 
+    public Result updateOneOfficeSupply(Integer id){
+        JsonNode json = request().body().asJson();
+
+        if(json == null) {
+            return badRequest(Json.toJson(new DefaultErrorMessage(11,"Expecting Json data")));
+        }
+
+        if(id == null){
+            return badRequest(Json.toJson(new DefaultErrorMessage(12,"Missing Parameter (ID)")));
+        }
+
+        OfficeSupply officeSupply = new OfficeSupply(json.findPath("name").textValue(),json.findPath("price").intValue(),json.findPath("condition").intValue(),json.findPath("description").textValue(), Date.valueOf(json.findPath("creationDate").asText()),json.findPath("image").textValue(),"office supply");
+        //Properties checker
+        officeSupply.setId(json.findPath("id").intValue());
+        return updateOneOfficeSupply(officeSupply);
+    }
+
+
+    public Result updateOneOfficeSupply(OfficeSupply officeSupply){
+
+        try (
+                Connection connection = db.getConnection();
+                PreparedStatement articleStatement = connection.prepareStatement("UPDATE articles SET name = ?, description = ?, condition = ?, price = ?, creationdate = ?, image = ? WHERE article_id = ?", Statement.RETURN_GENERATED_KEYS);
+        ){
+            articleStatement.setString(1,officeSupply.getName());
+            articleStatement.setString(2,officeSupply.getDescription());
+            articleStatement.setInt(3,officeSupply.getCondition());
+            articleStatement.setInt(4,officeSupply.getPrice());
+            articleStatement.setDate(5,officeSupply.getCreationDate());
+            articleStatement.setString(6,officeSupply.getImage());
+            articleStatement.setInt(7,officeSupply.getId());
+
+            int affectedRows = articleStatement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Updating office supply failed, no rows affected.");
+            }
+
+
+        }catch (SQLException e){
+            return badRequest(Json.toJson(new DefaultErrorMessage(e.getErrorCode(),e.getMessage())));
+        }
+        return ok(Json.toJson(officeSupply));
+
+    }
+
+
     public Result getAllOfficeSupplies(){
 
         try (Connection connection = db.getConnection()){
