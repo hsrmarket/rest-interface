e58c98f1cdb18f74c56ee7b25a2bdd303c537b39
Elias Brunner <elias_brunner@gmx.ch>
2017-04-27 11:22:26 +0200

    updateOneAccount(id) works

--- app/controllers/AccountController.java
+++ app/controllers/AccountController.java
@@ -95,6 +95,78 @@ public class AccountController extends Controller {
     }
 
 
+    public Result updateOneAccount(Integer id){
+        JsonNode json = request().body().asJson();
+
+        if(json == null) {
+            return badRequest(Json.toJson(new DefaultErrorMessage(11,"Expecting Json data")));
+        }
+
+        if(id == null){
+            return badRequest(Json.toJson(new DefaultErrorMessage(12,"Missing Parameter (ID)")));
+        }
+
+        Address address = new Address(json.findPath("street").textValue(),json.findPath("streetNr").textValue(),json.findPath("zip").asInt(),json.findPath("city").textValue());
+        Account account = new Account(json.findPath("studentId").asInt(),json.findPath("firstname").textValue(),json.findPath("lastname").textValue(),address,json.findPath("email").textValue(),json.findPath("telephone").textValue(),json.findPath("password").textValue(),json.findPath("admin").asBoolean());
+
+        //Properties checker
+        address.setId(json.get("address").findPath("id").asInt());
+        account.setId(json.findPath("id").asInt());
+
+        return updateOneAccount(account);
+    }
+
+
+    private Result updateOneAccount(Account account){
+        try {
+            connection = db.getConnection();
+            PreparedStatement addressStatement = connection.prepareStatement("UPDATE address SET streetname = ?, streetnumber = ?, zip = ?, city = ? WHERE address_id = ?", Statement.RETURN_GENERATED_KEYS);
+
+            addressStatement.setString(1,account.getAddress().getStreet());
+            addressStatement.setString(2,account.getAddress().getStreetNr());
+            addressStatement.setInt(3,account.getAddress().getZip());
+            addressStatement.setString(4,account.getAddress().getCity());
+            addressStatement.setInt(5,account.getAddress().getId());
+
+            int affectedRows = addressStatement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Updating address failed, no rows affected.");
+            }
+
+            ResultSet addressGeneratedKeys = addressStatement.getGeneratedKeys();
+            PreparedStatement accountStatement = connection.prepareStatement("UPDATE accounts SET firstname = ?, lastname = ?, studentid = ?, email = ?, tel = ?, pw = ?, isadmin = ? WHERE account_id = ?", Statement.RETURN_GENERATED_KEYS);
+
+            if (addressGeneratedKeys.next()) {
+                accountStatement.setString(1,account.getFirstname());
+                accountStatement.setString(2,account.getLastname());
+                accountStatement.setInt(3,account.getStudentId());
+                accountStatement.setString(4,account.getEmail());
+                accountStatement.setString(5,account.getTelephone());
+                accountStatement.setString(6,account.getPassword());
+                accountStatement.setBoolean(7,account.isAdmin());
+                accountStatement.setInt(8,account.getId());
+
+                accountStatement.executeUpdate();
+
+            }
+            else {
+                throw new SQLException("Updating account failed, no ID obtained.");
+            }
+
+        }catch (SQLException e){
+            return badRequest(Json.toJson(new DefaultErrorMessage(e.getErrorCode(),e.getMessage())));
+        }finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                return badRequest(Json.toJson(new DefaultErrorMessage(e.getErrorCode(),e.getMessage())));
+            }
+        }
+        return ok(Json.toJson(account));
+    }
+
+
     public Result deleteAccount(Integer id){
         try {
             connection = db.getConnection();
