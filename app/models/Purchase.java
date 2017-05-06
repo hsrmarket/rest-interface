c97ffbce265a9243c0e998a8c9f0c5619544eea4
Urs Forrer <mail@ursforrer.ch>
2017-05-06 11:47:26 +0200

    We are now saving the seller on a purchase
    We are now saving the seller on a purchase

--- app/models/Purchase.java
+++ app/models/Purchase.java
@@ -8,14 +8,16 @@ public class Purchase {
     private Integer id;
     private Article article;
     private Account buyer;
+    private Account seller;
     private Boolean isCompleted;
     private Date purchaseDate;
 
-    public Purchase(Article article, Account buyer, Boolean isCompleted, Date purchaseDate) {
+    public Purchase(Article article, Account buyer, Boolean isCompleted, Date purchaseDate, Account seller) {
         this.article = article;
         this.buyer = buyer;
         this.isCompleted = isCompleted;
         this.purchaseDate = purchaseDate;
+        this.seller = seller;
     }
 
     public Integer getId() {
@@ -41,6 +43,10 @@ public class Purchase {
     public void setBuyer(Account buyer) {
         this.buyer = buyer;
     }
+
+    public Account getSeller() {return seller;}
+
+    public void setSeller(Account seller) {this.seller = seller;}
 
     public Boolean getCompleted() {
         return isCompleted;
